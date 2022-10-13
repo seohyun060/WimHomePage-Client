d122dbad1860b09ecd06670b8ca10a2c4d3d931a
JMK1007 <coxld010@naver.com>
2022-10-13 12:24:37 +0900

    기본 셋팅

---
+++ src/assets/images.ts
@@ -0,0 +1,28 @@
+const images = {
+  logoHeader: '/assets/images/ic-logo-header.png',
+  logoFooter: '/assets/images/ic-logo-w-footer.png',
+  buisiness: {
+    hardware: '/assets/images/img-bf-hardware.png',
+    webApp: '/assets/images/img-bf-webapp.png',
+    ai: '/assets/images/img-bf-ai.png',
+  },
+  partners: {
+    logo1: '/assets/images/logo/logo-01.png',
+    logo2: '/assets/images/logo/logo-02.png',
+    logo3: '/assets/images/logo/logo-03.png',
+    logo4: '/assets/images/logo/logo-04.png',
+    logo5: '/assets/images/logo/logo-05.png',
+    logo6: '/assets/images/logo/logo-06.png',
+    logo7: '/assets/images/logo/logo-07.png',
+    logo8: '/assets/images/logo/logo-08.png',
+    logo9: '/assets/images/logo/logo-09.png',
+    logo10: '/assets/images/logo/logo-10.png',
+    logo11: '/assets/images/logo/logo-11.png',
+  },
+  contactBg: '/assets/images/img-bg.png',
+  downloadBig: '/assets/images/ic-download-24.png',
+  downloadSmall: '/assets/images/ic-download-18.png',
+  mainBg: '/assets/images/101.gif',
+};
+
+export default images;
