74b98a1f6b8df88e3aa432afe3d5f06f1680850a
JMK1007 <coxld010@naver.com>
2023-01-16 17:26:25 +0900

    fix(Main): fix style

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,19 +5,29 @@ import './styles/main.styles.css';
 const Main = () => {
   return (
     <div className='main-root' id='main'>
+      <div className='background'>
+        <video
+          className='bg-video'
+          src={images.mainVideo}
+          controls={false}
+          autoPlay
+          muted
+          loop
+        />
+        <div className='layer1' />
+        <div className='layer2' />
+      </div>
       <div className='main-container'>
-        <div className='slogan-container'>
-          <h1 className='eng-h1 white'>{'Imagination\nBecomes Reality'}</h1>
-          <span className='kor-h3 gray2'>{`여러가지 고객지향적 소프트웨어를 제작하여 고객들이 즐길 수 있는  공간, 편의를 
-누릴 수 있는 공간들을 제작하는 소프트웨어 전문회사 윔 입니다. `}</span>
+        <h1 className='eng-h1 white'>{'Imagination\nBecomes Reality'}</h1>
+        <span className='kor-h3 gray2 description'>{`여러가지 고객지향적 소프트웨어를 제작하여 고객들이 즐길 수 있는  공간, 편의를 \n누릴 수 있는 공간들을 제작하는 소프트웨어 전문회사 윔 입니다. `}</span>
+        <button className='btn-solid-round'>{'프로젝트 문의하기'}</button>
+      </div>
+      <div className='scroll-container'>
+        <span className='eng-h3 white'>{'Scroll down'}</span>
+        <div className='scroll-anim'>
+          <div className='ball' />
         </div>
       </div>
-      <video
-        className='bg-video'
-        src={images.mainVideo}
-        controls={false}
-        autoPlay={true}
-      />
     </div>
   );
 };
