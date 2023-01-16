118566f4339f732e46081d8d849cdcef60bc2f60
JMK1007 <coxld010@naver.com>
2023-01-16 15:38:43 +0900

    fix(Header): fix style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,20 +8,20 @@ const Header = () => {
       <div className='header-container'>
         <div className='logo'>
           <a href='#main'>
-            <img src={images.logoHeader} alt='logo' />
+            <img src={images.logoHeaderW} alt='logo' />
           </a>
         </div>
         <ul className='gnb-container'>
-          <li className='item'>
+          <li className='item eng-h3'>
             <a href='#business'>{'Business Field'}</a>
           </li>
-          <li className='item'>
+          <li className='item eng-h3'>
             <a href='#history'>{'History'}</a>
           </li>
-          <li className='item'>
+          <li className='item eng-h3'>
             <a href='#partners'>{'Partners'}</a>
           </li>
-          <li className='item'>
+          <li className='item eng-h3'>
             <a href='#contact'>{'Contact Us'}</a>
           </li>
         </ul>
