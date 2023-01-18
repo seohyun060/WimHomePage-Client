b8dcb7bc59eb1c4cfa9578402077d4b802e2421d
JMK1007 <coxld010@naver.com>
2023-01-18 16:20:39 +0900

    fix

--- src/components/Contact/components/ContactDetail.tsx
+++ src/components/Contact/components/ContactDetail.tsx
@@ -1,7 +1,9 @@
+import images from '@assets/images';
 import React from 'react';
+import '../styles/contact.detail.styles.css';
 
 const ContactDetail = () => {
-  return <div>ContactDetail</div>;
+  return <div className='contact-detail-root'></div>;
 };
 
 export default ContactDetail;
