a50f668158499ce3cdf8ca3f0e1becc407a88cdf
Noah <seohyun060@naver.com>
2023-02-20 19:18:01 +0900

    adjust

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,7 +5,6 @@ import HistoryContainer from '@components/History/containers/HistoryContainer';
 import MainContainer from '@components/Main/containers/MainContainer';
 import PartnersContainer from '@components/Partners/containers/PartnersContainer';
 import React from 'react';
-import ContactContainer from '@components/Contact/containers/ContactContainer';
 const Home = () => {
   return (
     <>
